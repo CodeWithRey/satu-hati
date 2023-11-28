5f1607411df045cf4524aa8a06b3f87f0e8bd7bc
CodeWithRey <rpolo2069@gmail.com>
2023-11-28 19:47:23 +0700

    [add] menambahkan routing ke edit-profile

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,12 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'age' => ['numeric'],
+            'birthday' => ['date'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'password' => ['required', 'confirmed'],
         ];
     }
 }
