d13a01c6e25ea879faffc68d3d2816c8815158e4
CodeWithRey <rpolo2069@gmail.com>
2023-11-28 21:10:35 +0700

    [add] menambahkan gender ke view

--- resources/views/pages/editprofile.blade.php
+++ resources/views/pages/editprofile.blade.php
@@ -69,8 +69,9 @@
                     <div class="mt-2">
                         <select id="gender" name="gender"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                            <option>Laki-laki</option>
-                            <option>Perempuan</option>
+`                            @foreach ($genders as $gender)
+                                <option value="{{ $gender->id }}">{{ $gender->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
