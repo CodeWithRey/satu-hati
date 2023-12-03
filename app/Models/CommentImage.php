70cdf3af12ba988b931cdf80331ed069fb8aa159
CodeWithRey <rpolo2069@gmail.com>
2023-12-03 17:25:47 +0700

    [fix] memperbaiki penamaan file image untuk comment dan post

---
+++ app/Models/CommentImage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentImage extends Model
+{
+    use HasFactory;
+}
