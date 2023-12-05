483c4e14ccc0416fedc09f5c44c3ee06b1b61217
Junia0806 <juniavitasari0806@gmail.com>
2023-12-05 21:01:05 +0700

    [example] contoh buat file error

---
+++ resources/views/errors/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - Not Found</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            text-align: center;
+            margin: 150px;
+        }
+
+        h1 {
+            font-size: 5em;
+            color: #333;
+        }
+
+        p {
+            font-size: 1.2em;
+            color: #666;
+        }
+    </style>
+</head>
+<body>
+    <h1>404</h1>
+    <p>Oops! The page you are looking for might be in another castle.</p>
+</body>
+</html>
