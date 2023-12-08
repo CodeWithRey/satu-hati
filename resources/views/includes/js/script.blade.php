e9a40d849a4bdf51360bc9f055d50a457d1fde2a
CodeWithRey <rpolo2069@gmail.com>
2023-12-08 11:50:58 +0700

    [fix] memperbaiki source css dan js

--- resources/views/includes/js/script.blade.php
+++ resources/views/includes/js/script.blade.php
@@ -1,5 +1,6 @@
 <script src="{{ asset('assets/js/flowbite.min.js') }}"></script>
-@vite('resources/js/app.js')
+{{-- @vite('resources/js/app.js') --}}
+<script src="{{ asset('build/assets/app-aa379cfa.js') }}"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
     integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
