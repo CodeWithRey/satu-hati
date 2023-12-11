97723e05df85e13aa9a5f39ac3571b172af609bb
CodeWithRey <rpolo2069@gmail.com>
2023-12-11 19:58:18 +0700

    [add] menambahkan alert ketika login dan register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -63,6 +63,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('post.index');
+        return redirect()->route('post.index')->with('toast_success', 'Berhasil untuk mendaftar akun.');
     }
 }
